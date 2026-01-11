28282e5d9fe08b5a0e0b63cf90a3d2e911a1b2ce
Alex Tran <455406+byalextran@users.noreply.github.com>
2026-01-11 16:23:03 -0600

    updated footer links

--- _data/metadata.js
+++ _data/metadata.js
@@ -6,9 +6,7 @@ export default {
 	author: {
 		name: "Alex Tran",
 		url: "https://www.alextran.org",
-		// Social links (used in footer) - add your URLs below
-		twitter: "", // e.g., "https://twitter.com/yourusername"
-		github: "",  // e.g., "https://github.com/yourusername"
-		linkedin: "" // e.g., "https://linkedin.com/in/yourusername"
+		github: "https://github.com/byalextran",
+		instagram: "https://instagram.com/byalextran"
 	}
 }
