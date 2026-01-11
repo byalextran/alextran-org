2bf441e8be2092a92ead1d2dda9028324a0fa300
Alex Tran <455406+byalextran@users.noreply.github.com>
2026-01-11 17:24:23 -0600

    removed unnecessary metadata.js variables

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,12 +1,4 @@
 export default {
 	title: "Alex Tran",
-	url: "https://www.alextran.org",
-	language: "en",
-	description: "TODO",
-	author: {
-		name: "Alex Tran",
-		url: "https://www.alextran.org",
-		github: "https://github.com/byalextran",
-		instagram: "https://instagram.com/byalextran"
-	}
+	language: "en"
 }
