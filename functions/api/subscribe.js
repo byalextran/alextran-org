2ba1fc6c7ac3fbdf76de294ce538d56eafae5fa8
Alex Tran <455406+byalextran@users.noreply.github.com>
2026-01-13 16:16:13 -0600

    implemented subscribe form

---
+++ functions/api/subscribe.js
@@ -0,0 +1,43 @@
+export async function onRequestPut(context) {
+  var json = await context.request.json();
+
+  const response = await fetch(
+    `https://api.emailoctopus.com/lists/${context.env.NEWSLETTER_LIST_ID}/contacts`,
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${context.env.NEWSLETTER_API_KEY}`
+      },
+      body: JSON.stringify(json)
+    }
+  );
+
+  var responseHeaders = {
+    status: response.status,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  var responseMessage = {
+    message: context.env.SUBSCRIPTION_SUCCESS_MESSAGE
+  };
+
+  const responseJSON = await response.json();
+
+  switch (response.status) {
+    case 200: // already subscribed
+    case 201: // new subscriber
+      break;
+    default: // assume an error
+      if (responseJSON.hasOwnProperty('errors')) {
+        responseMessage.message = responseJSON.errors[0]['detail'];
+      } else {
+        responseMessage.message = context.env.SUBSCRIPTION_FAILED_MESSAGE;
+      }
+  }
+
+  return new Response(JSON.stringify(responseMessage), responseHeaders);
+}
