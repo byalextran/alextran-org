bd0c6d998d81ef97e345513725c5c68ed7d4cc3c
Alex Tran <455406+byalextran@users.noreply.github.com>
2026-01-13 21:45:00 -0600

    created partials for meta output. added OG and other helpful tags.

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,4 +1,7 @@
 export default {
 	name: "Alex Tran",
-	language: "en"
+	language: "en",
+	url: "https://www.alextran.org",
+	description: "A field guide for the thoughtful practitioner. Actionable spirituality and lifestyle optimization that make both deeply satisfying.",
+	image: "/images/abstract-jellyfish.png"
 }
