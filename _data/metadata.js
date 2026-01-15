4b2f5e5176852295043f9437c5097f07c15db6a5
Alex Tran <455406+byalextran@users.noreply.github.com>
2026-01-14 19:24:58 -0600

    change PNG to JPG

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,5 +3,5 @@ export default {
 	language: "en",
 	url: "https://www.alextran.org",
 	description: "Exploring the intersection of practical faith, useful life hacks, and the occasional rabbit hole. On a quest to make life and faith feel deeply satisfying.",
-	image: "/images/abstract-jellyfish.png"
+	image: "/images/abstract-jellyfish.jpg"
 }
